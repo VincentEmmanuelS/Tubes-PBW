5f137c5ec65e642375fcf5534707e9277ef46d64
VINCENT EMMANUEL SUWARDY <i22067@ftis>
2024-12-12 10:47:36 +0700

    add: login page & sign up page, fix: post method, ToDo: homepage

--- src/main/java/com/tubes/pbw/controller/PageController.java
+++ src/main/java/com/tubes/pbw/controller/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
 
 @Controller
-public class HomepageController {
+public class PageController {
     
     @GetMapping("/")
     public String homepage() {
@@ -15,6 +15,16 @@ public class HomepageController {
     @GetMapping("/onboarding")
     public String onboarding() {
         return "onboarding";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
     }
 
 }
