14885a8ec0eba53b18541a807ba4d38ae1eb4bf8
Alexander Vinchent N <alexvinchent312.talenta@gmail.com>
2024-12-16 10:08:58 +0700

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tubes-PBW
-Dibuat untuk memenuhi Tugas Besar mata kuliah Pemrograman Berbasis Web
---
+++ src/main/java/com/tubes/pbw/manualEntry/ManualEntryController.java
@@ -0,0 +1,12 @@
+package com.tubes.pbw.manualEntry;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManualEntryController {    
+    @GetMapping("/")
+    public String halamanManualEntry(){
+        return "/ManualEntry/ManualEntry";
+    }
+}
